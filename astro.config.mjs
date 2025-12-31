a1bb74fb76aae149830ab2f1ba6998c539366034
Safvan Husain <developer.safvan@gmail.com>
2025-12-31 12:42:41 +0530

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/bliz-demo/',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
